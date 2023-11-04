17723ba34e1a8525fd8a0155c4f5639ac4a92801
Nachanon Nuanphet <peumza1150@gmail.com>
2023-11-04 21:44:12 +0700

    init branch

--- check_gpu.py
+++ check_gpu.py
@@ -1,2 +1,6 @@
 import tensorflow as tf
-print(tf.test.is_gpu_available())
+print(tf.__version__)
+print(tf.config.list_physical_devices('GPU'))
+
+# import torch
+# print(torch.cuda.is_available())
