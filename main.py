49f1c342fc6b6620422cba70d19b74372aca397f
Nachanon Nuanphet <peumza1150@gmail.com>
2023-11-30 16:37:25 +0700

    remove labels

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ cap = cv2.VideoCapture('test.mp4')
 # cap = cv2.VideoCapture(0)
 focus = FocusPoint()
 # personModel = torch.hub.load('ultralytics/yolov5', 'custom', 'models\yolov5s_openvino_model', device='cpu')
-personModel = YOLO(r"models\yolov8n.pt").cuda()
+personModel = YOLO(r"models\yolov8n.pt").cpu()
 fasionModel = YOLO(r"models\best.pt").cuda()
 
 def draw_circle(event, x, y, flags, param):
@@ -30,13 +30,13 @@ def main():
                 print("Ignoring empty camera frame.")
                 break
             # person_imgs = OnnxDetect(image, personModel)
-            person_imgs = YoloDetect(image, personModel, classes=[0])
+            person_imgs = YoloDetect(image, personModel)
             for person_img in person_imgs:
                 YoloDetect(person_img, fasionModel, conf=0.8)
-            # face_imgs, bounding_boxs = face_detect(image, person_imgs)
-            # if face_imgs and bounding_boxs:
-            #     focus_x, focus_y = focus.get_focus()
-            #     FaceMesh(image, face_imgs, person_imgs, bounding_boxs, focus_x, focus_y)
+            face_imgs, bounding_boxs = face_detect(image, person_imgs)
+            if face_imgs and bounding_boxs:
+                focus_x, focus_y = focus.get_focus()
+                FaceMesh(image, face_imgs, person_imgs, bounding_boxs, focus_x, focus_y)
             # find fps
             end = time.time()
             fps = int(1 / (end - start))
