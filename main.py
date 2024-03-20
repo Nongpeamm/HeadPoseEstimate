6d5272080568f0cbe41106460f7db910bd5a812e
Nongpeamm <Beambike2@gmail.com>
2024-03-20 15:13:59 +0700

    add head pose vector

--- main.py
+++ main.py
@@ -11,7 +11,16 @@ from pathlib import Path
 from colors import Color
 from load_model import YoloDetectWithTracker
 from insightface.app import FaceAnalysis
+import numpy as np
 
+model_points = np.array([
+                            (0.0, 0.0, 0.0),             # Nose tip
+                            (0.0, -330.0, -65.0),        # Chin
+                            (-225.0, 170.0, -135.0),     # Left eye left corner
+                            (225.0, 170.0, -135.0),      # Right eye right corne
+                            (-150.0, -150.0, -125.0),    # Left Mouth corner
+                            (150.0, -150.0, -125.0)      # Right mouth corner
+                        ])
 
 video_writer = cv2.VideoWriter(
         "test_beam.mp4", cv2.VideoWriter_fourcc(*'mp4v'), 30, (640, 480))
@@ -49,6 +58,7 @@ def detect_person_cam1(model: DetectFasion, image:cv2.Mat, id_dict: dict, DICT_M
         video_writer.write(cv2.resize(image, (640, 480), interpolation=cv2.INTER_AREA))
 
 def detect_face_cam2(person_detect_model: YOLO, image:cv2.Mat, face_detect_model: FaceAnalysis):
+    
     # person_imgs, _ = YoloDetectWithTracker(
     #         image, person_detect_model, sort_tracker, conf=0.4)  # return person images
         # bbox_list, face_landmark = face_detect_model.detect(image)
@@ -63,20 +73,50 @@ def detect_face_cam2(person_detect_model: YOLO, image:cv2.Mat, face_detect_model
         # x, y = face_landmark[0][2]  # nose
         # # cv2.circle(person_img, (int(x), int(y)), 1, Color.red, 2) #error CUDA
         # cv2.rectangle(image, (int(x) - 10, int(y) - 10), (int(x) + 10, int(y) + 10), Color.red, 2)
-    
+        
+    # init camera coordinate
+    focal_length = image.shape[1]
+    center = (image.shape[1] / 2, image.shape[0] / 2)
+    cv2. circle(image, (int(center[0]), int(center[1])), 6, Color.pink, 6)
+    camera_matrix = np.array(
+                        [[focal_length, 0, center[0]],
+                        [0, focal_length, center[1]],
+                        [0, 0, 1]], dtype = "double"
+                        )
+    dist_coeffs = np.zeros((4,1)) # Assuming no lens distortion
     resized_img = cv2.resize(image, (640, 640), interpolation=cv2.INTER_AREA)
     face_imgs = face_detect_model.get(resized_img)
-    for face_img in face_imgs:
-        landmark = face_img.landmark_3d_68[30] # nose
-        x, y, z = landmark
-        x = x * image.shape[1] / resized_img.shape[1]
-        y = y * image.shape[0] / resized_img.shape[0]
-        z = z * image.shape[1] / resized_img.shape[1]
-        cv2.circle(image, (int(x), int(y)), 2, Color.red, 6)
+    
+    # face_img.landmark_3d_68[8] # chin
+    # face_img.landmark_3d_68[30] # nose
+    # face_img.landmark_3d_68[45] # left eye
+    # face_img.landmark_3d_68[36] # right eye
+    # face_img.landmark_3d_68[48] # left mouth
+    # face_img.landmark_3d_68[54] # right mouth
+    
+    for face_img in face_imgs:        
+        idx = [30, 8, 45, 36, 48, 54]
+        face_2d = []
+        face_3d = []
+        for i in idx:
+            x, y, z = face_img.landmark_3d_68[i]
+            x = x * image.shape[1] / resized_img.shape[1]
+            y = y * image.shape[0] / resized_img.shape[0]
+            z = z * image.shape[1] / resized_img.shape[1]
+            cv2.circle(image, (int(x), int(y)), 2, Color.red, 6)
+            
+            face_2d.append((int(x), int(y)))
+            face_3d.append((int(x), int(y), int(z)))
+        face_2d = np.array(face_2d, dtype='double')
+        (_, rotation_vector, translation_vector) = cv2.solvePnP(model_points, face_2d, camera_matrix, dist_coeffs, flags=cv2.SOLVEPNP_ITERATIVE)
+        (nose_end_point2D, _) = cv2.projectPoints(np.array([(0.0, 0.0, 1200.0)]), rotation_vector, translation_vector, camera_matrix, dist_coeffs)
         
-
-def main(model: DetectFasion, face_detect_model: FaceAnalysis, person_detect_model: YOLO):
-    cap1 = cv2.VideoCapture('test.mp4')
+        p1 = (int(face_2d[0][0]), int(face_2d[0][1]))
+        p2 = (int(nose_end_point2D[0][0][0]), int(nose_end_point2D[0][0][1]))
+        cv2.line(image, p1, p2, (255,0,0), 2)
+        
+def main(model: DetectFasion, face_detect_model: FaceAnalysis, person_detect_model: YOLO):    
+    cap1 = cv2.VideoCapture('eye.mp4')
     cap2 = cv2.VideoCapture(0)
     id_dict = {}
     frame_skip = 5  # ตั้งค่า frame skip
@@ -91,7 +131,7 @@ def main(model: DetectFasion, face_detect_model: FaceAnalysis, person_detect_mod
             cap1.grab()
             cap2.grab()
 
-        # detect_person_cam1(model, frame1, id_dict, 0)
+        detect_person_cam1(model, frame1, id_dict, 0)
         detect_face_cam2(person_detect_model, frame2, face_detect_model)
 
         if frame1.shape[0] != frame2.shape[0]:
